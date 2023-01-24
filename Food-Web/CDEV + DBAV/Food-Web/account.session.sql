c9c63728e462866af430c9eb35b357257ab9821e
Javen <107395637+Javen05@users.noreply.github.com>
2023-01-24 17:06:29 +0800

    Delete Food-Web directory
    Signed-off-by: Javen <107395637+Javen05@users.noreply.github.com>

--- Food-Web/CDEV + DBAV/Food-Web/account.session.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users`(
-    `UID` BIGINT NOT NULL,
-    `username` LINESTRING NOT NULL,
-    `email` LINESTRING NOT NULL,
-    `phone` BIGINT NOT NULL,
-    `password` LINESTRING NOT NULL
-);
